6ad54a067544833af0e54d68406a1fa5eb4808d0
Giorgio Acquati <gioac96@gmail.com>
2021-09-26 14:52:59 +0200

    Fix server message forwarding and apply naming corrections

--- NETCore Chat/ChatShared/SDK/Messages/IMessage.cs
+++ NETCore Chat/ChatShared/SDK/Messages/IMessage.cs
@@ -4,8 +4,8 @@ namespace ChatShared.SDK.Messages
 {
     [ProtoContract]
     [ProtoInclude(101, typeof(MyNameIsMessage))]
-    [ProtoInclude(102, typeof(SendText))]
-    [ProtoInclude(103, typeof(ForwardText))]
+    [ProtoInclude(102, typeof(SendTextMessage))]
+    [ProtoInclude(103, typeof(ForwardTextMessage))]
     public interface IMessage
     {
         
